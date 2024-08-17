27b104d52c3ca9e6583a1b9f0a2b6fee6f5bae55
sudarshansanap9 <129634260+sudarshansanap9@users.noreply.github.com>
2024-08-17 17:43:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import './App.css';
+import Homepage from './component/Homepage';
+import Navbar from './component/Navbar';
+import React, { useState } from 'react';
+import SmallNavbar from './component/SmallNavbar';
+
+
+function App() {
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      
+      // document.title = 'TextUtils - Dark Mode';
+      // setInterval(() => {
+      //   document.title = 'TextUtils is Amazing Mode';
+      // }, 2000);
+      // setInterval(() => {
+      //   document.title = 'Install TextUtils Now';
+      // }, 1500);
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      
+      // document.title = 'TextUtils - Light Mode';
+    }
+  }
+  return (
+    <>
+    <Navbar title="VIRUSTOTAL" mode={mode} toggleMode={toggleMode} />
+     <Homepage mode={mode}/>
+     <SmallNavbar mode={mode}/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Navbar.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function Navbar(props) {
+  
+  const getOffcanvasStyle = () => {
+    const width = window.innerWidth;
+    return width < 700
+      ? { backgroundColor: props.mode === 'light' ? 'white' : '#343a40' }
+      : {};
+  };
+  
+  const getResponsiveStyle = () => {
+    const width = window.innerWidth;
+    let responsiveStyle = {
+      backgroundColor: props.mode === 'light' ? 'white' : 'white',
+      fontFamily: "'Times New Roman', Times, serif",
+     
+    };
+
+    if (480 < width) {
+      responsiveStyle.width = '1000px';
+    } else if (width <= 480) {
+      
+      responsiveStyle.width = '300px';
+    }
+
+    return responsiveStyle;
+  };
+  return (
+    <nav className={`navbar navbar-expand-lg navbar-${props.mode} fixed-top`} style={{ backgroundColor: `${props.mode === 'light' ? 'Gainsboro' : 'DarkSlateBlue'}` }}>
+    <div className="container-fluid">
+      <a className="navbar-brand" href="/">{props.title}</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div className="offcanvas-header" style={{ backgroundColor: `${props.mode === 'light' ? 'Gainsboro' : 'DarkSlateBlue'}` }}>
+        <h5 className="offcanvas-title" id="offcanvasNavbarLabel">{props.title}</h5>
+        <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div className="offcanvas-body" style={getOffcanvasStyle()}>
+        <ul className="navbar-nav justify-content-end flex-grow-1 pe-3 ">
+        <ul className="navbar-nav me-auto  ">
+            <li className="nav-item d-flex flex-grow-1">
+              
+              <form className="d-flex mt-1" role="search">
+              <input
+      className="form-control me-2"
+      type="search"
+      placeholder="URL, IP address, domain or file hash"
+      aria-label="Search"
+      style={getResponsiveStyle()} />
+                <button className="btn btn-outline-success" type="submit">Search</button>
+              </form>
+            </li>
+          </ul>
+          
+            
+            <li className="nav-item item">
+              <div className={` mx-2 text-${props.mode === 'light' ? 'dark' : 'light'} mt-2`}>
+              <i className="fa-solid fa-circle-half-stroke" onClick={props.toggleMode}></i>
+              </div>
+            </li>
+          
+        </ul>
+        
+      </div>
+    </div>
+  </div>
+</nav>
+  );
+}
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  mode: PropTypes.string.isRequired,
+  toggleMode: PropTypes.func.isRequired,
+};
+
+Navbar.defaultProps = {
+  title: 'Set title here',
+};
+
+
+
+
+
+
+
+
